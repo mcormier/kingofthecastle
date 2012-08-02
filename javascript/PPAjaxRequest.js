18744fbe9d854f2c0e03244a88e6d95ef4aee986
Matthieu Cormier <matthieu.cormier@concertia.com>
2012-08-02 13:50:06 -0300

    Add Ajax wrapper object

---
+++ javascript/PPAjaxRequest.js
@@ -0,0 +1,42 @@
+// http://en.wikipedia.org/wiki/XMLHttpRequest
+
+if (typeof XMLHttpRequest == "undefined") {
+  XMLHttpRequest = function () {
+    try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+      catch (e) {}
+    try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+      catch (e) {}
+    try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+      catch (e) {}
+    //Microsoft.XMLHTTP points to Msxml2.XMLHTTP and is redundant
+    throw new Error("This browser does not support XMLHttpRequest.");
+  };
+
+  XMLHttpRequest.DONE = 4;
+}
+
+
+function PPAjaxRequest(successCallback, errorCallback) {
+  if (errorCallback == null || successCallback == null) {
+    throw new Error("PPAjaxRequest missing required parameter.");
+  }
+  this.xmlHttp = new XMLHttpRequest();
+
+  var xmlHttp = this.xmlHttp;
+  this.xmlHttp.onreadystatechange =
+    function () {
+       if( xmlHttp.readyState == XMLHttpRequest.DONE ) {
+          if( xmlHttp.status == 200) {
+            successCallback(xmlHttp);
+        } else {
+            errorCallback(xmlHttp);
+        }
+      }
+    }
+
+  }
+
+PPAjaxRequest.prototype.getResource = function(resourceName) {
+    this.xmlHttp.open("GET",resourceName, true);
+    this.xmlHttp.send(null);
+}
