8d7d7e3eb9fe080b677c5af97797658240ea4b26
Matthieu Cormier <matthieu.cormier@concertia.com>
2012-08-14 14:52:44 -0300

    Decouple PPCanvasView object type from Application object

--- javascript/PPApplication.js
+++ javascript/PPApplication.js
@@ -1,22 +1,28 @@
-function PPApplication(canvasName, editor) {
+function PPApplication(canvasName, editor, canvasView) {
+  this.documentLoaded = false;
   this.canvasName = canvasName;
 
-  if (editor != null ) {
-    this.setEditController(editor);
-  }
+  if (editor != null ) { this.setEditController(editor); }
+  if (canvasView != null) {  this.setCanvasView(canvasView);}
+}
+
+PPApplication.prototype.listenForOnLoad = function () {
+  var self = this;
+  // TODO IE... understands attachEvent not addEventListener...
+  window.addEventListener("load", function () {self.onLoad();} , false);
+}
+
+PPApplication.prototype.onLoad = function () {
+  this.documentLoaded = true;
+  this.linkCanvasViewToElement();
 }
 
 PPApplication.prototype.setEditController = function (controller) {
   this.editController = controller;
-
-  if(this.tournament != null ) {
-    this.editController.setTournament(this.tournament);
-  }
+  if(this.tournament != null ) { this.editController.setTournament(this.tournament);}
 }
 
-PPApplication.prototype.setCanvasView = function (view) {
-  this.canvasView = view;
-
+PPApplication.prototype.linkCanvasViewToElement = function () {
   var canvas = $(this.canvasName);
   var self = this;
 
@@ -25,18 +31,20 @@ PPApplication.prototype.setCanvasView = function (view) {
   canvas.onclick = function handleClick (e) {
     self.canvasView.handleClick(e);
   }
+}
+
+PPApplication.prototype.setCanvasView = function (view) {
+  this.canvasView = view;
+  if (this.documentLoaded) { this.linkCanvasViewToElement(); }
+  if (this.tournament != null) { this.canvasView.setTournament(this.tournament);}
 
 }
 
 PPApplication.prototype.setTournament = function ( tObj ) {
   this.tournament = tObj;
 
-  this.setCanvasView( new PPCanvasView(this.tournament) );
-
-  if (this.editController != null ) {
-    this.editController.setTournament(this.tournament);
-  }
-
+  if (this.canvasView != null) { this.canvasView.setTournament(this.tournament);}
+  if (this.editController != null ) {this.editController.setTournament(this.tournament);}
 }
 
 PPApplication.prototype.titleChanged = function (id, evt) {
@@ -45,5 +53,5 @@ PPApplication.prototype.titleChanged = function (id, evt) {
 
 
 PPApplication.prototype.redraw = function () {
-  this.canvasView.redraw();
+  if (this.canvasView) {this.canvasView.redraw();}
 }
