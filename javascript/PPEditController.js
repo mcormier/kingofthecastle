e5e45d65b399226ea7924d1d2a03d82540d26a61
Matthieu Cormier <matthieu.cormier@concertia.com>
2012-08-15 13:32:20 -0300

    create abstract methods.

--- javascript/PPEditController.js
+++ javascript/PPEditController.js
@@ -10,7 +10,10 @@ PPEditController.prototype.setTournament = function(tournament) {
 }
 
 PPEditController.prototype.setMatchWinner = function() {throw new Error("Abstract method PPEditController.setMatchWinner"); }
-
+PPEditController.prototype.disablePlayerTextFields = function(state) {throw new Error("Abstract method PPEditController.disablePlayerTextFields"); }
+PPEditController.prototype.disableWinnerRadioButtons = function (state) {  throw new Error("Abstract method PPEditController.disableWinnerRadioButtons");}
+PPEditController.prototype.setEditorValues = function (match) {throw new Error("Abstract method PPEditController.setEditorValues");}
+PPEditController.prototype.setRadioValues = function (match) { throw new Error("Abstract method PPEditController.setRadioValues"); }
 
 PPEditController.prototype.updatePlayer1 = function (id) {
   var value = $(id).value;
@@ -49,46 +52,7 @@ PPEditController.prototype.selectMatch = function ( selectedMatch /* PPMatch */ 
      this.setRadioValues(selectedMatch);
 }
 
-PPEditController.prototype.disablePlayerTextFields = function (state)  {
-       $("player1TextField").disabled = state;
-       $("player2TextField").disabled = state;
-}
 
-PPEditController.prototype.disableWinnerRadioButtons = function (state) {
-       $("player1WinnerRadio").disabled = state;
-       $("player2WinnerRadio").disabled = state;
-       // Clear the checked state as we disable...
-       if ( state == true ) {
-         $("player1WinnerRadio").checked = false;
-         $("player2WinnerRadio").checked = false;
-       }
-}
 
-PPEditController.prototype.setEditorValues = function (match) {
-       if ( match == null ) {
-         $("player1TextField").value = "";
-         $("player2TextField").value = "";
-       } else {
-         $("player1TextField").value = match.getPlayer1Name();
-         $("player2TextField").value = match.getPlayer2Name();
-       }
-}
 
-PPEditController.prototype.setRadioValues = function (match) {
-       if ( match == null) {
-         return;
-       }
 
-       if (match.completed()) {
-         var winVal = match.winnerOrdinal();
-         if ( winVal == 1 ) {
-            $("player1WinnerRadio").checked = true;
-         }
-         if ( winVal == 2 ) {
-            $("player2WinnerRadio").checked = true;
-         }
-       } else {
-          $("player1WinnerRadio").checked = false;
-          $("player2WinnerRadio").checked = false;
-       }
-}
